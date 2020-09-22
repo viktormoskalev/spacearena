62bb9592db61e3e5d7ed70931c6af352c5363a78
viktormoskalev <neonchilkk@gmail.com>
2020-09-22 21:19:40 +0300

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+document.querySelector("button").addEventListener("click",(function(){document.querySelector("body").classList.toggle("open")}));
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+document.querySelector('button').addEventListener('click', function(){
+    document.querySelector('body').classList.toggle('open');
+  });
